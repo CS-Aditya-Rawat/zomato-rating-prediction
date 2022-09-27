13b08d121e2b38d5a036c673912d085f29b3ffad
CsAdityaRawat <adityainferno4812@gmail.com>
2022-09-27 20:20:30 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# Zomato Restaurants Analysis and Predicting their ratings.
+
+## Main.ipynb consist of all the codes
+
+**parameters of json**
+
+- `online_order` : Whether Online Ordering is available in restaurant or not.
+- `book_table` :
+- `votes` :
+- `cost` :
+- `location` :
+- `rest_type` :
+- `type` :
+
+### Json Request Form
+
+```
+{
+    "online_order": "Yes",
+    "book_table": "Yes",
+    "votes" : 775,
+    "cost" : 800.0,
+    "location" : "Banashankari",
+    "rest_type" : "",
+    "type" : ""
+}
+```
---
+++ main.py
@@ -0,0 +1,54 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import pickle
+import pandas as pd
+import json
+from utils import known_location, known_rest_type, known_type
+
+app = FastAPI()
+
+
+def preprocess_data(df):
+    col_to_OHE = ['online_order', 'book_table']
+    for col in col_to_OHE:
+        df[col] = df[col].map({True : 1, False : 0})
+        df[col] = pd.to_numeric(df[col])
+        
+        
+    location = pd.Categorical(df['location'], categories=known_location)
+    location_dummies = pd.get_dummies(location)
+    
+    rest_type = pd.Categorical(df['rest_type'], categories=known_rest_type)
+    rest_type_dummies = pd.get_dummies(rest_type)
+
+    typ = pd.Categorical(df['type'], categories=known_type)
+    type_dummies = pd.get_dummies(typ)
+    
+    df = pd.concat([df, location_dummies, rest_type_dummies, type_dummies], axis=1)
+    df = df.drop(['location', 'rest_type', 'type'], axis=1)
+    return df
+ 
+class RatingItem(BaseModel):
+    online_order:bool#"Yes",
+    book_table: bool #"Yes",
+    votes:float # 775,
+    cost:float #800.0,
+    location:str #"Banashankari",
+    rest_type: str # "Casual Dining",
+    type:str # "Buffet"
+
+with open('Decision_tree_model.pkl', 'rb') as f:
+    model = pickle.load(f)
+    
+@app.get("/")
+async def read_root():
+    return {"Hello":"World"}
+
+@app.post("/predict")
+async def rating_endpoint(item:RatingItem):
+    df = pd.DataFrame([item.dict().values()], columns=item.dict().keys())
+    df = preprocess_data(df)
+    # print(f"COLS OF DATAFRAME: {df.shape[1]}")
+    # print("Prediction", model.predict(df))
+    y_pred = round(model.predict(df)[0], 2)
+    return json.dumps({"Rating":y_pred})
